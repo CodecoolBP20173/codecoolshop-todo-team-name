3055e9df5c915ccdeb21acf917faab7fd3e890d8
koroknayakos <koroknayakos@gmail.com>
2018-05-02 14:30:40 +0200

    Tests created for ProductDao interface

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
@@ -27,32 +27,36 @@ public class ProductDaoMem implements ProductDao {
 
     @Override
     public void add(Product product) {
-        product.setId(data.size() + 1);
-        data.add(product);
+        product.setId(getData().size() + 1);
+        getData().add(product);
     }
 
     @Override
     public Product find(int id) {
-        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+        return getData().stream().filter(t -> t.getId() == id).findFirst().orElse(null);
     }
 
     @Override
     public void remove(int id) {
-        data.remove(find(id));
+        getData().remove(find(id));
     }
 
     @Override
     public List<Product> getAll() {
-        return data;
+        return getData();
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return data.stream().filter(t -> t.getSupplier().equals(supplier)).collect(Collectors.toList());
+        return getData().stream().filter(t -> t.getSupplier().equals(supplier)).collect(Collectors.toList());
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return data.stream().filter(t -> t.getProductCategory().equals(productCategory)).collect(Collectors.toList());
+        return getData().stream().filter(t -> t.getProductCategory().equals(productCategory)).collect(Collectors.toList());
+    }
+
+    public List<Product> getData() {
+        return data;
     }
 }
--- src/main/test/com/codecool/shop/dao/ProductDaoTest.java
+++ src/main/test/com/codecool/shop/dao/ProductDaoTest.java
@@ -1,7 +1,79 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDaoTest {
 
+    ProductDao productDao = ProductDaoMem.getInstance();
+    Supplier testSupplier = new Supplier("test", "description");
+    ProductCategory testProductCat = new ProductCategory("name", "department", "description");
+    Product testProduct = new Product("test", 1, "USD", "description", testProductCat, testSupplier);
+
+    @BeforeEach
+    public void init() {
+        productDao.add(testProduct);
+    }
+
+    @AfterEach
+    public void after() {
+        productDao.remove(1);
+    }
+
+
+    @Test
+    public void testIsProductDaoNotNull() {
+        assertNotNull(productDao);
+    }
+
+    @Test
+    public void testIsProductNotNull() {
+        List<Product> products = productDao.getAll();
+        assertNotNull(products.get(0));
+    }
+
+    @Test
+    public void testIsProductAdded() {
+        productDao.add(new Product("test", 2, "USD", "description", new ProductCategory("name", "department", "description"), new Supplier("test", "description")));
+        List<Product> products = productDao.getAll();
+        assertEquals(2, products.get(products.size() - 1).getId());
+        productDao.remove(products.get(products.size() - 1).getId());
+    }
+
+    @Test
+    public void testIsProductFound() {
+        List<Product> products = productDao.getAll();
+        assertEquals(products.get(0), productDao.find(products.get(0).getId()));
+    }
+
+    @Test
+    public void testIsProductRemoved() {
+        Product p = new Product("test", 4, "USD", "description", new ProductCategory("name", "department", "description"), new Supplier("test", "description"));
+        productDao.add(p);
+        int id = p.getId();
+        productDao.remove(id);
+        assertNull(productDao.find(id));
+    }
+
+    @Test
+    public void testAreAllProductsGot() {
+        assertEquals(1, productDao.getAll().size());
+    }
+
+    @Test
+    public void testIsProductGotBySupplier() {
+        assertEquals(testProduct, productDao.getBy(testSupplier).get(0));
+    }
+
+    @Test
+    public void testIsProductGotByCategory() {
+        assertEquals(testProduct, productDao.getBy(testProductCat).get(0));
+    }
 }
