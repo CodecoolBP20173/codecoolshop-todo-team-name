8be1f55679111548fbc02ed64787f997cf4e59bc
koroknayakos <koroknayakos@gmail.com>
2018-05-02 14:31:53 +0200

    Removed redundant getData method from ProductDaoMem

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoMem.java
@@ -27,36 +27,32 @@ public class ProductDaoMem implements ProductDao {
 
     @Override
     public void add(Product product) {
-        product.setId(getData().size() + 1);
-        getData().add(product);
+        product.setId(data.size() + 1);
+        data.add(product);
     }
 
     @Override
     public Product find(int id) {
-        return getData().stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
     }
 
     @Override
     public void remove(int id) {
-        getData().remove(find(id));
+        data.remove(find(id));
     }
 
     @Override
     public List<Product> getAll() {
-        return getData();
+        return data;
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return getData().stream().filter(t -> t.getSupplier().equals(supplier)).collect(Collectors.toList());
+        return data.stream().filter(t -> t.getSupplier().equals(supplier)).collect(Collectors.toList());
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return getData().stream().filter(t -> t.getProductCategory().equals(productCategory)).collect(Collectors.toList());
-    }
-
-    public List<Product> getData() {
-        return data;
+        return data.stream().filter(t -> t.getProductCategory().equals(productCategory)).collect(Collectors.toList());
     }
 }
